92cb453fdf41475c847ed39cf4168247ad2f20fa
Giles Bradford <gbradford@zipcodes-MacBook-Pro-4.local>
2020-02-07 12:59:22 -0500

    I started working on WriteIFs first but as the compiler is not working, I am starting with WriteLoops.

--- WriteIFs.java
+++ WriteIFs.java
@@ -11,16 +11,20 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
-     
+        if (player1.isAlive() == false) {
+            displayGameOver(player);
+        }
     }
     
     public String thermoSTAT(int room) {
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        if (temperature(room) < 70) {
+            heatOn();
+        } else {
+            coolOn();
+        }
         return this.ss;
     }
 
@@ -30,13 +34,17 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
-
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+        if (fuelLevel < 0.08) {
+            refuel();
+        }
     }
 
 
@@ -58,7 +66,7 @@ public class WriteIFs
    * Constructor for objects of class WriteIFs
    */
   public WriteIFs()
-  {
+    {
       // initialise instance variables
       x = 0;
       tt_t = 0;
@@ -66,7 +74,7 @@ public class WriteIFs
       ss = "";
       oo1 = 61;
       oo2 = 49;
-  }
+    }
 
     // associated routines
     public boolean isAlive(boolean p) {
